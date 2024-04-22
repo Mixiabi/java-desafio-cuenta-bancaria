195fba7e4c03a14cf1dc11645ebe768cfdfd0146
Mxiabi1996 <michelle_montoya96@outlook.com>
2024-04-21 19:35:44 -0500

    agregando instancia para opciones y bucle while

--- src/Desafio.java
+++ src/Desafio.java
@@ -1,8 +1,13 @@
+import java.util.*;
 public class Desafio {
     public static void main(String[] args){
+
+//        Scanner ingresoDeDatos = new Scanner(System.in);
+        Scanner ingresoDeOpciones = new Scanner(System.in);
         String nombreCliente = "Tony Stark";
         String tipoDeCuenta = "Corriente";
         Double saldo = 1599.99;
+        int opciones = 0;
 
         String consultaDeDatos = """
                 *******************************
@@ -10,16 +15,26 @@ public class Desafio {
                 Nombre del Cliente: %s
                 Tipo de cuenta: %s
                 Saldo Disponible: $ %1.2f
+                
                 """;
         System.out.printf(consultaDeDatos,nombreCliente,tipoDeCuenta,saldo);
 
         String principalMenu = """
+                
+                *********************************************
                 **Escriba el numero de la operacion deseada**
+     
                 1 - Consultar saldo
                 2 - Retirar
                 3 - Depositar
                 9 - Salir
                 """;
-        System.out.println(principalMenu);
+
+        while (opciones !=9){
+            System.out.println(principalMenu);
+            opciones = ingresoDeOpciones.nextInt();
+
+
+        }
     }
 }
