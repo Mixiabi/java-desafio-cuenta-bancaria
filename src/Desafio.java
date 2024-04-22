58f38bca1bb9aa51e3865c7c4571cb690182c56c
Mxiabi1996 <michelle_montoya96@outlook.com>
2024-04-22 17:31:05 -0500

    agregando ingreso de nuevo cliente

--- src/Desafio.java
+++ src/Desafio.java
@@ -1,33 +1,11 @@
 import java.util.*;
 public class Desafio {
     public static void main(String[] args){
-
-
-
-
         Scanner ingresoDeOpciones = new Scanner(System.in);
-
-        String nombreCliente = "Tony Stark";
-        String tipoDeCuenta = "Corriente";
-        double saldo = 1599.99;
-
-        double saldoRetirado;
-        double saldoIngresado;
-        int opciones = 0;
-
-        String consultaDeDatos = """
-                *******************************
-                
-                Nombre del Cliente: %s
-                Tipo de cuenta: %s
-                Saldo Disponible: $ %1.2f
-                
-                """;
-        System.out.printf(consultaDeDatos,nombreCliente,tipoDeCuenta,saldo);
-
+        Scanner ingresoDeDatos = new Scanner(System.in);
+//MENU PRINCIPAL
         String principalMenu = """
                 *********************************************
-                
                 **Escriba el numero de la operacion deseada**
                 1 - Consultar saldo
                 2 - Retirar
@@ -35,62 +13,46 @@ public class Desafio {
                 9 - Salir
                 """;
 
-        while (opciones !=9){
-            System.out.println(principalMenu);
-            opciones = ingresoDeOpciones.nextInt();
+//VARIABLES - CLIENTE POR DEFECTO
+        String nombreCliente = "Tony Stark";
+        String tipoDeCuenta = "Corriente";
+        double saldo = 1599.99;
 
-            if (opciones == 1){
-                //MUESTRA EL SALDO
+//VARIABLES DE RETIRO E INGRESO DE NUEVO Y DEFAULT CLIENTE
+        double saldoRetirado, saldoNuevoRetirado;
+        double saldoIngresado, saldoNuevoIngresado;
+        int opciones = 0;
 
-                System.out.println('\n' + "Su saldo actualizado es de: $ " + saldo + '\n');
-
-            } else if (opciones == 2) {
-
-                System.out.println("\nCuanto dinero retirara?\n");
-                saldoRetirado = ingresoDeOpciones.nextDouble();
-
-                if (saldo > saldoRetirado){
-                    saldo = saldo - saldoRetirado;
-                    System.out.println('\n' + "Tu nuevo saldo es de: $ " + saldo + '\n');
-
-                }else {
-                    System.out.println("Saldo insuficiente\n");
-                }
-
-            } else if (opciones == 3) {
-                System.out.println("\nCuanto dinero depositara?\n");
-                saldoIngresado = ingresoDeOpciones.nextDouble();
-
-                saldo = saldo + saldoIngresado;
-                System.out.println("\nTu nuevo saldo es de: $ " + saldo + '\n');
-            } else if (opciones == 9) {
-                System.out.println("\nFinalizacion el programa. Gracias por usar nuestros servicios");
-            }else {
-                System.out.println("\nOpcion no valida\n");
-            }
-        }
-
-
-
-//      PARA INGRESAR NUEVO CLIENTE
-
-        Scanner ingresoDeDatos = new Scanner(System.in);
-
-        String nombreNuevoCliente;
-        String cuentaNuevoCliente;
-        double saldoNuevoCliente;
-
-        String consultaNuevosDatos = """
+        String consultaDeDatos = """
+                *********************************************
                 \nNombre del Cliente: %s
                 Tipo de cuenta: %s
                 Saldo Disponible: $ %1.2f
                 """;
 
-        System.out.println("Es usted un cliente nuevo? (Si/No)");
-        String pregunta = ingresoDeDatos.nextLine();
+// VARIABLES - NUEVO CLIENTE
+        String nombreNuevoCliente;
+        String cuentaNuevoCliente;
+        double saldoNuevoCliente;
+
+        String consultaNuevosDatos = """
+                \n*********************************************
+                \nNombre del Cliente: %s
+                Tipo de cuenta: %s
+                Saldo Disponible: $ %1.2f
+                """;
+
+//PREGUNTA SI ES CLIENTE NUEVO O NO
+        String pregunta = """
+                \n*********************************************
+                \nEs usted un cliente nuevo? (Si/No)
+                """;
+        System.out.println(pregunta);
+        String respuesta = ingresoDeDatos.nextLine();
         String clienteNuevo = "Si";
 
-        if (pregunta.equalsIgnoreCase(clienteNuevo)){
+//CLIENTE NUEVO
+        if (respuesta.equalsIgnoreCase(clienteNuevo)) {
 
             System.out.println("\nIngrese su nombre: ");
             nombreNuevoCliente = ingresoDeDatos.nextLine();
@@ -99,12 +61,70 @@ public class Desafio {
             System.out.println("\nIngrese su saldo disponible:");
             saldoNuevoCliente = ingresoDeDatos.nextDouble();
 
-            System.out.printf(consultaNuevosDatos,nombreNuevoCliente,cuentaNuevoCliente,saldoNuevoCliente);
+            System.out.printf(consultaNuevosDatos, nombreNuevoCliente, cuentaNuevoCliente, saldoNuevoCliente);
 
-        }else {
-            System.out.println("programa finalizado");
+            while (opciones != 9) {
+                System.out.println('\n' + principalMenu);
+                opciones = ingresoDeOpciones.nextInt();
 
+                if (opciones == 1) {
+                    //MUESTRA EL SALDO
+                    System.out.println("\nSu saldo actualizado es de: $ " + saldoNuevoCliente + '\n');
+                    //AGREGAR NUEVO RETIRO
+                } else if (opciones == 2) {
 
+                    System.out.println("\nCuanto dinero retirara?\n");
+                    saldoNuevoRetirado = ingresoDeOpciones.nextDouble();
+                    if (saldoNuevoCliente > saldoNuevoRetirado) {
+                        saldoNuevoCliente -= saldoNuevoRetirado;
+                        System.out.println('\n' + "Tu nuevo saldo es de: $ " + saldoNuevoCliente + '\n');
+                    } else {
+                        System.out.println("\nSaldo insuficiente\n");
+                    }
+                    //AGREGAR NUEVO INGRESO
+                } else if (opciones == 3) {
+                    System.out.println("\nCuanto dinero depositara?\n");
+                    saldoNuevoIngresado = ingresoDeOpciones.nextDouble();
+                    saldoNuevoCliente += saldoNuevoIngresado;
+                    System.out.println("\nTu nuevo saldo es de: $ " + saldoNuevoCliente + '\n');
+                } else if (opciones == 9) {
+                    System.out.println("\nFinalizacion el programa. Gracias por usar nuestros servicios");
+                } else {
+                    System.out.println("\nOpcion no valida\n");
+                }
+            }
+
+//CLIENTE DEFAULT
+        } else {
+            System.out.printf(consultaDeDatos+'\n', nombreCliente, tipoDeCuenta, saldo);
+            while (opciones != 9) {
+                System.out.println(principalMenu);
+                opciones = ingresoDeOpciones.nextInt();
+                if (opciones == 1) {
+                    //MUESTRA EL SALDO
+                    System.out.println('\n' + "Su saldo actualizado es de: $ " + saldo + '\n');
+                    //AGREGAR NUEVO RETIRO
+                } else if (opciones == 2) {
+                    System.out.println("\nCuanto dinero retirara?\n");
+                    saldoRetirado = ingresoDeOpciones.nextDouble();
+                    if (saldo > saldoRetirado) {
+                        saldo = saldo - saldoRetirado;
+                        System.out.println('\n' + "Tu nuevo saldo es de: $ " + saldo + '\n');
+                    } else {
+                        System.out.println("Saldo insuficiente\n");
+                    }
+                    //AGREGAR NUEVO INGRESO
+                } else if (opciones == 3) {
+                    System.out.println("\nCuanto dinero depositara?\n");
+                    saldoIngresado = ingresoDeOpciones.nextDouble();
+                    saldo += saldoIngresado;
+                    System.out.println("\nTu nuevo saldo es de: $ " + saldo + '\n');
+                } else if (opciones == 9) {
+                    System.out.println("\nFinalizacion el programa. Gracias por usar nuestros servicios");
+                } else {
+                    System.out.println("\nOpcion no valida\n");
+                }
+            }
         }
     }
 }
